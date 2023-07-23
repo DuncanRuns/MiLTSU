f574f94bf7e1e5155b8628824a5088b09a089618
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-07-23 12:34:36 -0300

    Java 8 Compat for JitPack

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ Just some basic Minecraft Loot Table Sequence Utilities.
 
 Random sequence stuff implemented in C:
 [random_sequence_c_stuff.zip](https://github.com/DuncanRuns/MiLTSU/files/11916009/random_sequence_c_stuff.zip)
+
+# Include in your own project
+
+We can use JitPack to include the projects directly from GitHub:
+
+```groovy
+repositories {
+    maven { url 'https://jitpack.io' }
+}
+```
+And include MiLTSU in your dependencies:
+```groovy
+dependencies {
+    implementation 'com.github.DuncanRuns:MiLTSU:-SNAPSHOT'
+}
+```
--- src/main/java/me/duncanruns/miltsu/randomsequence/BarteringRandomSequence.java
+++ src/main/java/me/duncanruns/miltsu/randomsequence/BarteringRandomSequence.java
@@ -1,29 +1,30 @@
 package me.duncanruns.miltsu.randomsequence;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BarteringRandomSequence extends RandomSequence {
-    private static List<Entry> entries = List.of(
-            new Entry("minecraft:book", 0, 0, 5, false, true),
-            new Entry("minecraft:iron_boots", 0, 0, 8, false, true),
-            new Entry("minecraft:potion", 0, 0, 8, false, false),
-            new Entry("minecraft:splash_potion", 0, 0, 8, false, false),
-            new Entry("minecraft:potion", 0, 0, 10, false, false),
-            new Entry("minecraft:iron_nugget", 10, 36, 10, true, false),
-            new Entry("minecraft:ender_pearl", 2, 4, 10, true, false),
-            new Entry("minecraft:string", 3, 9, 20, true, false),
-            new Entry("minecraft:quartz", 5, 12, 20, true, false),
-            new Entry("minecraft:obsidian", 1, 1, 40, false, false),
-            new Entry("minecraft:crying_obsidian", 1, 3, 40, true, false),
-            new Entry("minecraft:fire_charge", 1, 1, 40, false, false),
-            new Entry("minecraft:leather", 2, 4, 40, true, false),
-            new Entry("minecraft:soul_sand", 2, 8, 40, true, false),
-            new Entry("minecraft:nether_brick", 2, 8, 40, true, false),
-            new Entry("minecraft:spectral_arrow", 6, 12, 40, true, false),
-            new Entry("minecraft:gravel", 8, 16, 40, true, false),
-            new Entry("minecraft:blackstone", 8, 16, 40, true, false)
+    private static final List<LootEntry> ENTRIES = Arrays.asList(
+            new LootEntry("minecraft:book", 0, 0, 5, false, true),
+            new LootEntry("minecraft:iron_boots", 0, 0, 8, false, true),
+            new LootEntry("minecraft:potion", 0, 0, 8, false, false),
+            new LootEntry("minecraft:splash_potion", 0, 0, 8, false, false),
+            new LootEntry("minecraft:potion", 0, 0, 10, false, false),
+            new LootEntry("minecraft:iron_nugget", 10, 36, 10, true, false),
+            new LootEntry("minecraft:ender_pearl", 2, 4, 10, true, false),
+            new LootEntry("minecraft:string", 3, 9, 20, true, false),
+            new LootEntry("minecraft:quartz", 5, 12, 20, true, false),
+            new LootEntry("minecraft:obsidian", 1, 1, 40, false, false),
+            new LootEntry("minecraft:crying_obsidian", 1, 3, 40, true, false),
+            new LootEntry("minecraft:fire_charge", 1, 1, 40, false, false),
+            new LootEntry("minecraft:leather", 2, 4, 40, true, false),
+            new LootEntry("minecraft:soul_sand", 2, 8, 40, true, false),
+            new LootEntry("minecraft:nether_brick", 2, 8, 40, true, false),
+            new LootEntry("minecraft:spectral_arrow", 6, 12, 40, true, false),
+            new LootEntry("minecraft:gravel", 8, 16, 40, true, false),
+            new LootEntry("minecraft:blackstone", 8, 16, 40, true, false)
     );
-    private static final int TOTAL_WEIGHT = entries.stream().mapToInt(value -> value.weight).sum();
+    private static final int TOTAL_WEIGHT = ENTRIES.stream().mapToInt(value -> value.weight).sum();
 
     public BarteringRandomSequence(long worldSeed) {
         super(worldSeed, "minecraft:gameplay/piglin_bartering");
@@ -32,33 +33,56 @@ public class BarteringRandomSequence extends RandomSequence {
     public BarteringOut next() {
         int j = random.nextInt(TOTAL_WEIGHT);
 
-        for (Entry entry : entries) {
-            if ((j -= entry.weight) >= 0) continue;
-            return this.rollEntry(entry);
+        for (LootEntry lootEntry : ENTRIES) {
+            if ((j -= lootEntry.weight) >= 0) continue;
+            return this.rollEntry(lootEntry);
         }
         return null;
     }
 
-    private BarteringOut rollEntry(Entry entry) {
+    private BarteringOut rollEntry(LootEntry lootEntry) {
         int amount = 1;
-        if (entry.rollAmount) {
-            amount = random.nextInt(entry.min, entry.max);
+        if (lootEntry.rollAmount) {
+            amount = random.nextInt(lootEntry.min, lootEntry.max);
         }
-        if (entry.rollSS) {
+        if (lootEntry.rollSS) {
             random.nextInt(1); // Randomly choose out of a list of a single enchantment
             amount = random.nextInt(1, 3); // Choose soul speed level
         }
-        return new BarteringOut(entry.name, amount);
+        return new BarteringOut(lootEntry.name, amount);
     }
 
-    private record Entry(String name, int min, int max, int weight, boolean rollAmount, boolean rollSS) {
+    private static class LootEntry {
+        public final String name;
+        public final int min;
+        public final int max;
+        public final int weight;
+        public final boolean rollAmount;
+        public final boolean rollSS;
+
+        private LootEntry(String name, int min, int max, int weight, boolean rollAmount, boolean rollSS) {
+            this.name = name;
+            this.min = min;
+            this.max = max;
+            this.weight = weight;
+            this.rollAmount = rollAmount;
+            this.rollSS = rollSS;
+        }
     }
 
-    /**
-     * @param name the identifier for the Minecraft item
-     * @param amount if name is minecraft:iron_boots, this represents the level of soul speed on the boots, otherwise the amount of items in the stack
-     */
-    public record BarteringOut(String name, int amount) {
+    public static class BarteringOut {
+        public final String name;
+        public final int amount;
+
+        /**
+         * @param name   the identifier for the Minecraft item
+         * @param amount if name is minecraft:iron_boots, this represents the level of soul speed on the boots, otherwise the amount of items in the stack
+         */
+        public BarteringOut(String name, int amount) {
+            this.name = name;
+            this.amount = amount;
+        }
+
         @Override
         public String toString() {
             return name + ":" + amount;
